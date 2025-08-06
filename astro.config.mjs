d8e4bfa3380236927112c18ba393f2567fd20cc0
Daniel Savory <daniel.savory@marketplacer.com>
2025-08-06 20:58:46 +1000

    Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,22 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
-import react from "@astrojs/react";
+// import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-import db from "@astrojs/db";
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   // The `site` property specifies the base URL for your site.
   // Be sure to update this to your own domain (e.g., "https://yourdomain.com") before deploying.
-  site: "https://data-nova.vercel.app",
+  site: "https://merlets-au.vercel.app",
   prefetch: true,
   trailingSlash: "never",
   experimental: {
     clientPrerender: true,
   },
   integrations: [
-    react(),
+    // react(),
     markdoc({ ignoreIndentation: true, allowHTML: true }),
     ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()]),
   ],
