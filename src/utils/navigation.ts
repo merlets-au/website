60491c55869804defd9437122543a81551c92a52
Thu Savory <thu@yrovas.com>
2025-08-14 22:44:04 +1000

    add some pages, update nav and footer

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,6 +1,6 @@
 export const navigationLinks = [
   { href: "/about-merlets", label: "About" },
   { href: "/services", label: "Services Available" },
-
+  { href: "/trading", label: "Trading" },
   { href: "/contact", label: "Contact" },
 ];
